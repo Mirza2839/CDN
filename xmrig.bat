c144416abb0d935ee3833fc411b44139e32857f6
Mirza2839 <96509661+Mirza2839@users.noreply.github.com>
2026-07-31 10:04:12 +0700

    Update xmrig.bat

--- xmrig.bat
+++ xmrig.bat
@@ -15,11 +15,8 @@ echo [+] Menyiapkan folder dan pengecualian Windows Defender...
 powershell -NoProfile -ExecutionPolicy Bypass -Command "if (!(Test-Path '%INSTALL_DIR%')) { New-Item -ItemType Directory -Path '%INSTALL_DIR%' | Out-Null }; Add-MpPreference -ExclusionPath '%INSTALL_DIR%'" >nul 2>&1
 
 echo [+] Download dan Ekstrak XMRig...
-powershell -NoProfile -ExecutionPolicy Bypass -Command "$z='%TEMP%\x.zip'; iwr -useb 'https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-msvc-win64.zip' -OutFile $z; Expand-Archive $z '%TEMP%\x' -Force; Get-ChildItem '%TEMP%\x\*' -Recurse | Move-Item -Destination '%INSTALL_DIR%' -Force; Remove-Item $z, '%TEMP%\x' -Recurse -Force"
-
-echo [+] Membuat config.json...
-powershell -NoProfile -ExecutionPolicy Bypass -Command "$c = @{ api=@{id=$null; 'worker-id'=$null}; http=@{enabled=$false}; autosave=$true; background=$false; colors=$true; randomx=@{init=-1; 'init-avx2'=-1; mode='auto'; '1gb-pages'=$true; rdmsr=$true; wrmsr=$true; cache_qos=$false; numa=$true; scratchpad_prefetch_mode=1}; cpu=@{enabled=$true; 'huge-pages'=$true; 'hw-aes'=$true; priority=$null; asm=$true}; cuda=@{enabled=$false}; opencl=@{enabled=$false}; pools=@(@{algo='rx/0'; coin='monero'; url='gulf.moneroocean.stream:10128'; user='%WALLET%'; pass='%WORKER%'; 'rig-id'=$null; nicehash=$false; tls=$false}) }; $c | ConvertTo-Json -Depth 5 | Set-Content -Path '%INSTALL_DIR%\config.json' -Encoding UTF8"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "$z='%TEMP%\x.zip'; iwr -useb 'https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-msvc-win64.zip' -OutFile $z; Expand-Archive $z '%TEMP%\x' -Force; Get-ChildItem '%TEMP%\x\*' -Recurse | Move-Item -Destination '%INSTALL_DIR%' -Force; Remove-Item $z, '%TEMP%\x' -Recurse -Force" >nul 2>&1
 
 echo [+] Memulai XMRig di terminal ini...
 cd /d "%INSTALL_DIR%"
-xmrig.exe
+xmrig.exe -o gulf.moneroocean.stream:10128 -u %WALLET% -p %WORKER% -a rx/0 --donate-level 1
