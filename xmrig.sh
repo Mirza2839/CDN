51e52f0d2f5ec54019e243a7b99ca8da6baaf34c
Mirza2839 <96509661+Mirza2839@users.noreply.github.com>
2026-08-30 11:20:08 +0700

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-sudo pacman -S --needed git base-devel
+sudo pacman -S --needed --noconfirm git base-devel
+cd /tmp
+rm -rf yay 
 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg -si
-yay -Sy xmrig-donateless
-read -p "Name?: " nama_worker
+makepkg -si --noconfirm
+read -p "Mau kasih nama apa untuk worker ini?: " nama_worker
+yay -Sy --noconfirm xmrig-donateless
 xmrig -o pool.supportxmr.com:443 -u 48Cdv8BajGJJrgA4aFnQU64sugzQWzqKwLMMuKdjZdFvNAXF3P2WabWKjsfV3YMsb4BAaDDn5dYzea7HuyQ9RDvhAZQnNCx -k --tls -p "$nama_worker"
