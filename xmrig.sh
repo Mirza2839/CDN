1ab0b00eb6e85225b2fd3d22266153c276f12be3
Mirza2839 <96509661+Mirza2839@users.noreply.github.com>
2026-08-30 11:14:51 +0700

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+yay -Sy xmrig-donateless
 read -p "Name?: " nama_worker
 xmrig -o pool.supportxmr.com:443 -u 48Cdv8BajGJJrgA4aFnQU64sugzQWzqKwLMMuKdjZdFvNAXF3P2WabWKjsfV3YMsb4BAaDDn5dYzea7HuyQ9RDvhAZQnNCx -k --tls -p "$nama_worker"
