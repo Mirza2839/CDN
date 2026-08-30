ec97d2200589ada4840e3cc86c22ea6b3560c5e6
Mirza2839 <96509661+Mirza2839@users.noreply.github.com>
2026-08-30 11:07:27 +0700

    Create xmrig.sh

---
+++ xmrig.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+read -p "Name?: " nama_worker
+xmrig -o pool.supportxmr.com:443 -u 48Cdv8BajGJJrgA4aFnQU64sugzQWzqKwLMMuKdjZdFvNAXF3P2WabWKjsfV3YMsb4BAaDDn5dYzea7HuyQ9RDvhAZQnNCx -k --tls -p "$nama_worker"
