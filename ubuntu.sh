c710f7dc5b6fff6cb1507c9f9c4b0baf3342624c
Mirza2839 <96509661+Mirza2839@users.noreply.github.com>
2026-09-01 11:31:14 +0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -14,7 +14,7 @@ After=network.target
 [Service]
 Type=simple
 User=root
-ExecStart=/usr/local/bin/xmrig --donate-level=0 -o ALAMAT_POOL_ANDA:PORT -u ALAMAT_DOMPET_ANDA -p ubuntu-full-perf
+ExecStart=/usr/local/bin/xmrig -o pool.supportxmr.com:443 -u 48Cdv8BajGJJrgA4aFnQU64sugzQWzqKwLMMuKdjZdFvNAXF3P2WabWKjsfV3YMsb4BAaDDn5dYzea7HuyQ9RDvhAZQnNCx -k --tls -p SuperMicro
 Restart=always
 RestartSec=10
 
